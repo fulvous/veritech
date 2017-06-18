4de589c68840ba7a6edf10c52ee803d231089eb8
Leon Ramos <leon.ramos@gmail.com>
2017-06-18 17:46:41 -0500

    Added KEY_OU to KEY_LIST

--- bash/libs/dialog/easy-rsa.sh
+++ bash/libs/dialog/easy-rsa.sh
@@ -29,7 +29,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-KEY_LIST="(KEY_COUNTRY|KEY_PROVINCE|KEY_CITY|KEY_ORG|KEY_EMAIL|KEY_SIZE)"
+KEY_LIST="(KEY_COUNTRY|KEY_PROVINCE|KEY_CITY|KEY_ORG|KEY_EMAIL|KEY_SIZE|KEY_OU)"
 
 setup_easy-rsa () {
   
