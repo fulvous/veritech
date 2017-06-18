f965ef86628a25bfb3ea5a205c233b2de2870098
Leon Ramos <leon.ramos@gmail.com>
2017-06-18 03:27:25 -0500

    Added supported os function

--- bash/libs/os_detection.sh
+++ bash/libs/os_detection.sh
@@ -37,9 +37,24 @@ ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
 #    export VER=$DISTRIB_RELEASE
 #elif [ -f /etc/debian_version ]; then
 if [ -f /etc/debian_version ]; then
-    export OS=debian
-    export VER=$(cat /etc/debian_version)
+    OS=debian
+    VER=$(cat /etc/debian_version)
 elif [ -f /etc/redhat-release ]; then
-    export OS=redhat
-    export VER=$(cat /etc/redhat-release)
+    OS=redhat
+    VER=$(cat /etc/redhat-release | egrep '[0-9\.]+')
 fi
+
+supported_platform () {
+  if [ "${OS}" != "debian" && "${OS}" != "redhat" ] ; then
+    SUPPORTED=false
+  fi
+
+  if [ "${OS}" == "redhat" ] ; then
+    case "${VER}" in
+      "7") SUPPORTED=true
+        ;;
+      *) SUPPORTED=false
+        ;;
+    esac
+  fi
+}
