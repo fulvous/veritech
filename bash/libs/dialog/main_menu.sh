c600cc07cba9e45a32ca3de10416056c0cc48892
Leon Ramos <leon.ramos@gmail.com>
2017-06-20 16:51:38 -0500

    testing main_menu

--- bash/libs/dialog/main_menu.sh
+++ bash/libs/dialog/main_menu.sh
@@ -29,6 +29,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
 main_menu () {
   OPT=0
   while [ "$OPT" != "3" ]  do
